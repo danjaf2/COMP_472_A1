d4db9049a081c7492bf7a47a8208f057a66a4fc2
Dann <danjaf2@gmail.com>
2022-09-26 15:31:16 -0400

    Graphing

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,9 +1,15 @@
 import json
 import gzip
-import pandas as pd
+from pandas import DataFrame
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import math
+from collections import Counter
+
 
 
 
@@ -26,8 +32,36 @@ for i in range(length):
 
 
 
+
+
+
 vectorizer = CountVectorizer()
 posts_matrix = vectorizer.fit_transform(posts)
+Data = {'Emotions':emotions, 'Sentiments': sentiments}
+df = DataFrame(Data,columns=['Post','Emotions', 'Sentiments'])
+
+
+
+# distribution= [sentiments.count('positive')/length*100, sentiments.count('neutral')/length*100, sentiments.count('neutral')/length*100, sentiments.count('ambiguous')/length*100]
+# labels = 'Positive', 'Neutral', 'Negative', 'Ambiguous'
+# explode = (0, 0.1, 0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+#
+# plt.pie(distribution, explode=explode, labels=labels, autopct='%1.1f%%',
+#         shadow=True, startangle=90)
+# plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+#
+# plt.savefig("sentiments.png")
+
+
+plt.rcParams.update({'font.size': 3})
+
+plt.hist(emotions, bins=28, alpha=0.5)
+plt.title('Random Gaussian data (fixed number of bins)')
+plt.xlabel('variable X (20 evenly spaced bins)')
+plt.ylabel('count')
+
+plt.show()
+
 
 
 
