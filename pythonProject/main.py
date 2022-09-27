9d6777dcd5a2991e1514b495931aa2d5480453c3
Dann <danjaf2@gmail.com>
2022-09-27 12:14:40 -0400

    Did tasks 2.1 and 2.2

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -4,6 +4,7 @@ from pandas import DataFrame
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -29,19 +30,10 @@ for i in range(length):
     posts[i]=fullData[i][0]
     sentiments[i]= fullData[i][2]
 
-
-
-
-
-
-
 vectorizer = CountVectorizer()
 posts_matrix = vectorizer.fit_transform(posts)
-Data = {'Emotions':emotions, 'Sentiments': sentiments}
+Data = {'Post': posts,'Emotions':emotions, 'Sentiments': sentiments}
 df = DataFrame(Data,columns=['Post','Emotions', 'Sentiments'])
-
-
-
 # distribution= [sentiments.count('positive')/length*100, sentiments.count('neutral')/length*100, sentiments.count('neutral')/length*100, sentiments.count('ambiguous')/length*100]
 # labels = 'Positive', 'Neutral', 'Negative', 'Ambiguous'
 # explode = (0, 0.1, 0,0)  # only "explode" the 2nd slice (i.e. 'Hogs')
@@ -51,20 +43,21 @@ df = DataFrame(Data,columns=['Post','Emotions', 'Sentiments'])
 # plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 #
 # plt.savefig("sentiments.png")
+# plt.rcParams.update({'font.size': 3})
+#
+# plt.hist(emotions, bins=28, alpha=0.5)
+# plt.title('Emotions')
+# plt.xlabel('X')
+# plt.ylabel('Y')
+#
+# plt.show()
+
+print("The length of the vocabulary is "+str(len(vectorizer.vocabulary_)) )
 
 
-plt.rcParams.update({'font.size': 3})
+training_data, testing_data = train_test_split(df, test_size=0.2, shuffle=False)
 
-plt.hist(emotions, bins=28, alpha=0.5)
-plt.title('Emotions')
-plt.xlabel('X')
-plt.ylabel('Y')
-
-plt.show()
-
-
-
-
+print(training_data['Post'][1])
 
 
 
